3aa01ffe28bc186d87ff6ac1cf2be01130459ae2
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-12 12:42:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
+@@all = []  
   attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
+    @@all << self 
   end
+  
+  def self 
 
   def artist_name
     if self.artist == nil 
