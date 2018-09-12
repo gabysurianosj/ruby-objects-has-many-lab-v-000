7129aa586584e4c6c9bc81e231cde0bc479c61f3
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-12 12:47:00 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,14 @@
 class Post
-@@all = []
+  @@all = []
   attr_accessor :title, :author
  
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
